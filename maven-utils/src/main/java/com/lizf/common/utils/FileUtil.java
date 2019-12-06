98f47de24e6b3ab42cc84d20c2bf16c03b68a436
lizhenfang-lang <2109967063@qq.com>
2019-12-06 20:14:46 +0800

    文件工具类

--- maven-utils/src/main/java/com/lizf/common/utils/FileUtil.java
+++ maven-utils/src/main/java/com/lizf/common/utils/FileUtil.java
@@ -12,116 +12,140 @@ import java.util.List;
 import org.omg.CORBA.PUBLIC_MEMBER;
 
 public class FileUtil {
-	
+
 	/**
-	 *根据文件，截取扩展名 
+	 * 根据文件，截取扩展名
+	 * 
 	 * @param fileName
 	 * @return
 	 */
 	public static String getExtName(String fileName) {
-		//处理空异常
-		if (fileName==null || "".equals(fileName)) {
+		// 处理空异常
+		if (fileName == null || "".equals(fileName)) {
 			throw new RuntimeException("文件名不能为空");
 		}
-		if (fileName.indexOf(".")<=-1) {
-			throw new RuntimeException(fileName+":改文件名没有包含扩展名");
+		if (fileName.indexOf(".") <= -1) {
+			throw new RuntimeException(fileName + ":改文件名没有包含扩展名");
 		}
 		String extName = fileName.substring(fileName.lastIndexOf("."));
 		return extName;
 	}
-	
+
 	/**
-	 *获取系统当前用户目录 
+	 * 获取系统当前用户目录
+	 * 
 	 * @return
 	 */
 	public static String getSystemUserHome() {
 		return System.getProperty("user.home");
 	}
-	
+
 	/**
-	 *读取文件内容  
+	 * 读取文件内容
+	 * 
 	 * @param pathname
 	 * @return
 	 */
 	@SuppressWarnings("resource")
 	public static String readTextFileByLine(String pathname) {
 		BufferedReader br;
-		StringBuffer sb=new StringBuffer();
+		StringBuffer sb = new StringBuffer();
 		try {
-			br=new BufferedReader(new FileReader(new File(pathname)));
-		    do {
-		    	sb.append(br.readLine());
-		    	sb.append("\r\n");
-			} while (br.read()!=-1);
+			br = new BufferedReader(new FileReader(new File(pathname)));
+			do {
+				sb.append(br.readLine());
+				sb.append("\r\n");
+			} while (br.read() != -1);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 			return null;
-		}catch (IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 			return null;
 		}
 		return sb.toString();
 	}
+
 	/**
 	 * 按行读取文件内容到list集合
+	 * 
 	 * @param pathname
 	 * @return
 	 * @throws IOException
 	 */
 	@SuppressWarnings("resource")
-	public static List<String> readTextFileOfList(String pathname) throws IOException{
-		BufferedReader br;
-		List<String> strList=new ArrayList<>();
-		 try {
-			br=new BufferedReader(new FileReader(new File(pathname)));
+	public static List<String> readTextFileOfList(String pathname) throws IOException {
+		BufferedReader br = null;
+		List<String> strList = new ArrayList<>();
+		try {
+			br = new BufferedReader(new FileReader(new File(pathname)));
 			do {
 				strList.add(br.readLine());
-			} while (br.read()!=-1);
+			} while (br.read() != -1);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 			return null;
-		}catch (IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 			return null;
+		} finally {
+			StreamUtil.closeAll(br);
 		}
 		return strList;
 	}
+
 	/**
 	 * 递归删除文件
+	 * 
 	 * @param file
-	 * @return 
+	 * @return
 	 */
 	public static void deleteFile(File file) {
 		if (file.isDirectory()) {
-			File[] listFiles=file.listFiles();
-			for(File theFile:listFiles) {
+			File[] listFiles = file.listFiles();
+			for (File theFile : listFiles) {
 				deleteFile(theFile);
 			}
 			file.delete();
-		}else {
+		} else {
 			file.delete();
 		}
 	}
+
 	/**
 	 * 递归删除文件
+	 * 
 	 * @param theFile
 	 */
 	private static void deleteFile(String filePath) {
 		deleteFile(new File(filePath));
-		
+
 	}
 
 	public static String getFileSize(File file) {
-	   long length=file.length();
-	   return Math.round((length/1024.0))+"kb";
-	   
-    }
+		long length = file.length();
+		double len = length / 1024.0;
+		return String.format("%.2f", len) + "kb";
+
+	}
+
 	public static String getFileSize(String fileFullName) {
 		return getFileSize(new File(fileFullName));
-		
+
 	}
-	public static void main(String [] args) {
-		System.out.println(getFileSize("C:\\Users\\Administrator\\Desktop\\pom.xml"));
-		
+
+	public static void main(String[] args) {
+		// System.out.println(getFileSize("C:\\Users\\Administrator\\Desktop\\pom.xml"));
+		System.out.println();
+		System.out.println(getSystemTempDirectory());
+	}
+
+	/**
+	 * 操作系统临时目录
+	 * @return
+	 */
+	private static String getSystemTempDirectory() {
+		// TODO Auto-generated method stub
+		return System.getProperty("java.io.tmpdir");
 	}
 }

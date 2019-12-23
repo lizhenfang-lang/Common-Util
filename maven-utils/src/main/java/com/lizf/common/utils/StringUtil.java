38bf8f46209c9598577c343541f0d8903325d1c4
lizhenfang-lang <2109967063@qq.com>
2019-12-23 18:52:55 +0800

    修改工具类

--- maven-utils/src/main/java/com/lizf/common/utils/StringUtil.java
+++ maven-utils/src/main/java/com/lizf/common/utils/StringUtil.java
@@ -141,6 +141,14 @@ public class StringUtil {
 		}
 		return str;
 	}
+    /**
+	 * @Title: randomChineseString   
+	 * @Description: 返回参数length个中文汉字字符串，字符集必须在GB2312(相当于中文简体)范围内   
+	 * @param: @param length
+	 * @param: @return      
+	 * @return: String      
+	 * @throws
+	 */
     public static String randomChineseString(int length) {
 		String str = "";
 		for (int i = 0; i < length; i++) {
@@ -149,6 +157,13 @@ public class StringUtil {
 
 		return str;
 	}
+    /**
+	 * @Title: randomChineseName   
+	 * @Description: 返回中文姓名，必须以真实姓开头
+	 * @param: @return      
+	 * @return: String      
+	 * @throws
+	 */
     public static String randomChineseName() {
 		String[] surname = { "赵", "钱", "孙", "李", "周", "吴", "郑", "王", "冯", "陈", "褚", "卫", "蒋", "沈", "韩", "杨", "朱", "秦",
 				"尤", "许", "何", "吕", "施", "张", "孔", "曹", "严", "华", "金", "魏", "陶", "姜", "戚", "谢", "邹", "喻", "柏", "水", "窦",
@@ -186,9 +201,31 @@ public class StringUtil {
 		String name2 = randomChineseString(RandomUtil.random(1, 2));
 		return name1 + name2;
 	}
-    public static void main(String[] args) {
-		for (int i = 0; i < 10; i++) {
-			System.out.println(randomChineseName());
-		}
+    /**
+	 * 验证是否是URL
+	 * @param url
+	 * @return
+	 */
+	public static boolean isHttpUrl(String str){
+		 //转换为小写
+        str = str.toLowerCase();
+        String regex = "^((https|http|ftp|rtsp|mms)?://)"  //https、http、ftp、rtsp、mms
+                + "?(([0-9a-z_!~*'().&=+$%-]+: )?[0-9a-z_!~*'().&=+$%-]+@)?" //ftp的user@  
+               + "(([0-9]{1,3}\\.){3}[0-9]{1,3}" // IP形式的URL- 例如：199.194.52.184               
+                 + "|" // 允许IP和DOMAIN（域名） 或单域名
+                 + "[0-9a-z]*"  // 或单域名
+                 + "|" // 允许IP和DOMAIN（域名） 或单域名
+                 + "([0-9a-z_!~*'()-]+\\.)*" // 域名- www.  
+                 + "([0-9a-z][0-9a-z-]{0,61})?[0-9a-z]\\." // 二级域名  
+                + "[a-z]{2,6})" // first level domain- .com or .museum  
+                + "(:[0-9]{1,5})?" // 端口号最大为65535,5位数
+                + "((/?)|" // a slash isn't required if there is no file name  
+                + "(/[0-9a-z_!~*'().;?:@&=+$,%#-]+)+/?)$";  
+        return  str.matches(regex);	
+	}
+	
+	public static void main(String[] args) {
+		boolean httpUrl = isHttpUrl("https://127.0.0.1/a.html");
+		System.out.println(httpUrl);
 	}
 }
---
+++ maven-utils/src/test/java/com/lizf/common/utils/test/DateUtilTest.java
@@ -0,0 +1,14 @@
+package com.lizf.common.utils.test;
+
+import org.junit.Test;
+
+import com.lizf.common.utils.DateUtil;
+
+
+public class DateUtilTest {
+    @Test
+    public void test() {
+    	System.out.println(DateUtil.getAge("1998-02-13"));
+    	
+    }
+}
